8b7d50da5b464cf66ea3a87dd292f0f8400476bb
sirfrancois <sirfrancois4@gmail.com>
2021-02-02 12:53:22 -0600

    jQueryDrills

---
+++ app.js
@@ -0,0 +1,18 @@
+$('#btnSubmit').click(function() {
+    alert('a button was clicked broooo')
+});
+
+$('form').submitted(function() {
+   $('text').val()
+});
+
+div = document.createElement('div')
+div.className = 'div1'
+document.body.appendChild(div);
+
+
+h2 = document.createElement('h2')
+document.createTextNode('these are words')  addEventListener('')
+
+document.h2.appendChild(text)
+
